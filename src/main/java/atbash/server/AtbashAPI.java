0a3d9db874324784f84df7ab7b6077c51e8ddd2e
Dror <dror.shter@gmail.com>
2017-03-30 19:44:06 +0300

    little prince

--- src/main/java/atbash/server/AtbashAPI.java
+++ src/main/java/atbash/server/AtbashAPI.java
@@ -19,27 +19,12 @@ public class AtbashAPI
     @RequestMapping(value = "/getAllStages", method= GET, produces={"application/json; charset=UTF-8"})
     public Stage[] getAllStages() throws SQLException {
         return dal.getAllStages();
-    }/*
-    @GET
-    @Path("/getCount")
-    @Produces(MediaType.APPLICATION_JSON)ל
-    public String getCount()
-    {
+    }
+    @RequestMapping(value = "/getCount", method= GET, produces={"application/json; charset=UTF-8"})
+    public String getCount() throws SQLException {
         String ret;
         ret = String.valueOf(dal.getCount());
         System.out.println("Inside AtbashServerAPI: getCount" + ret);
         return ret;
-    }*/
-    //getStage is just for check, not for use.
-    @RequestMapping(value = "/getStage", method= GET, produces={"application/json; charset=UTF-8"})
-    public JSONObject getStage() throws JSONException
-    {
-        JSONObject json = new JSONObject();
-        json.put("number", 1);
-        json.put("question", "2");
-        json.put("clue", "3");
-        json.put("answer", "4");
-        return json;
     }
-    //example for parameters input : (@QueryParam("user") String user, @QueryParam("id") int id, @QueryParam("getLastNumber") String getLastNumber, @QueryParam("getAllStages") String getAllStages
 }
