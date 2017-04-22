1d369d23aed3c01df2abb9fa1b503b034c69493b
noambarsh <noambarsh@gmail.com>
2017-04-22 13:42:08 +0300

    questions, lombok

--- src/main/java/atbash/server/FacebookUser.java
+++ src/main/java/atbash/server/FacebookUser.java
@@ -1,32 +1,17 @@
 package atbash.server;
-//TODO:LOMBOK
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class FacebookUser
 {
     private String facebookID;
     private String name;
     private int currentStageNumber;
-
-    public FacebookUser()
-    {
-
-    }
-    public FacebookUser(String facebookID, String name, int currentStageNumber)
-    {
-        this.facebookID=facebookID;
-        this.name=name;
-        this.currentStageNumber=currentStageNumber;
-    }
-    public String getFacebookID()
-    {
-        return facebookID;
-    }
-    public String getName()
-    {
-        return name;
-    }
-    public int getCurrentStageNumber()
-    {
-        return currentStageNumber;
-    }
 
 }
--- src/main/java/atbash/server/Stage.java
+++ src/main/java/atbash/server/Stage.java
@@ -1,32 +1,15 @@
 package atbash.server;
-//TODO:LOMBOK
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
 public class Stage
 {
 	private int number;
-	public String question;
-	public String clue;
-	public String answer;
-	public Stage(int id, String question, String clue, String answer)
-	{
-		this.number = id;
-		this.question = question;
-		this.clue = clue;
-		this.answer = answer;
-	}
-	public int getNumber()
-	{
-		return number;
-	}
-	public String getQuestion()
-	{
-		return question;
-	}
-	public String getClue()
-	{
-		return clue;
-	}
-	public String getAnswer()
-	{
-		return answer;
-	}
+	private String question;
+    private String clue;
+    private String answer;
 }
